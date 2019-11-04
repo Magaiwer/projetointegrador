3c6e8955810779be01cc2b79ead033d17d8e4dd9
Tainá <tainaschaeffer@gmail.com>
2019-11-03 23:47:03 -0300

    Adicionadas opçoes de filtros

--- src/main/java/projetointegrador/controllers/DashboardsController.java
+++ src/main/java/projetointegrador/controllers/DashboardsController.java
@@ -1,14 +1,38 @@
 package projetointegrador.controllers;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
 import org.springframework.stereotype.Component;
+import projetointegrador.model.Project;
 
+import java.awt.event.ActionEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 @Component
 public class DashboardsController implements Initializable
 {
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private JFXComboBox<Project> comboProject;
+
+    @FXML
+    private JFXComboBox<?> comboRegion;
+
+    @FXML
+    private JFXButton btnAddFilter;
+
+    @FXML
+    void onAddFilter(ActionEvent event)
+    {
+
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources)
     {
