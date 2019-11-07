10414c6f70d7fbdf0ee02ae5d9fef2a3d8dcd048
Magaiver Santos <magaiver@tricon.inf.br>
2019-11-07 11:45:55 -0200

    Adicionada permissoes para email e detalhes

---
+++ src/main/resources/db/migration/V2.9__insert_permissoes_email_e_detalhes_projeto.sql
@@ -0,0 +1,7 @@
+insert into permission(role,description,form_id) values('btnEmail','Enviar email do projeto',10);
+
+insert into permission (role, description, form_id) values('btnDetailProject', 'Visualizar detalhes do projeto', 10);
+
+insert into group_permission values(1,37);
+insert into group_permission values(1,38);
+
