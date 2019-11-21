7a9ac503e24d2d3a8eb323a89e34d2ce17d5564e
Tainá <tainaschaeffer@gmail.com>
2019-11-20 23:58:40 -0300

    Ajuste nos arquivos do banco

--- src/main/resources/db/migration/V2.9__insert_permissoes_email_e_detalhes_projeto.sql
+++ src/main/resources/db/migration/V2.9__insert_permissoes_email_e_detalhes_projeto.sql
@@ -2,6 +2,6 @@ insert into permission(role,description,form_id) values('btnEmail','Enviar email
 
 insert into permission (role, description, form_id) values('btnDetailProject', 'Visualizar detalhes do projeto', 10);
 
-insert into group_permission values(1,37);
-insert into group_permission values(1,38);
+insert into group_permission values(1,41);
+insert into group_permission values(1,42);
 
